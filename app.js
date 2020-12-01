aa4a6e83f3cd97d1c480344d0d4edeeb2150945c
Mike Zapatrin <misha.zapatrin@yandex.ru>
2020-12-01 18:44:32 +0300

    init

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// const MongoClient = require("mongodb").MongoClient;
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-  
-
-// const userScheme = new Schema({
-//     id: Number,
-//     Name: String,
-//     imageUrl: String,
-//     price: Number
-// });
-   
-// const User = mongoose.model("Item",userScheme)
-// const findScheme = new Schema({title: String}, {_id: false});
-// const Item = mongoose.model("Item",findScheme)
-// mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
- 
-
-// Item.find({title:"Hats"}, function(err, docs){
-//     mongoose.disconnect();
-     
-//     if(err) return console.log(err);
-     
-//     console.log(docs);
-// });
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
@@ -39,18 +14,3 @@ User.find({title:"Hats"},'title', function(err, docs){
      
     console.log(Object(docs));
 });
-// mongoClient.connect(function(err, client){
-      
-//     const db = client.db("test");
-//     const collection = db.collection("items");
- 
-//     if(err) return console.log(err);
-      
-//     collection.find({title:"Jackets"}).project({items: 1,_id: 0}).toArray(function(err, results){
-                 
-//         results.forEach(element => {
-//             console.log(element)
-//         })
-//         client.close();
-//     });
-// });
